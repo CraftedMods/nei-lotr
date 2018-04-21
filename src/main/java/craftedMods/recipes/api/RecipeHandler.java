3bb18f1a34573ef072dcc8cf7b5073d66cc7f496
CraftedMods <craftedMods@t-online.de>
2018-04-21 13:38:03 +0200

    Improved some javadoc comments

--- src/main/java/craftedMods/recipes/api/RecipeHandler.java
+++ src/main/java/craftedMods/recipes/api/RecipeHandler.java
@@ -8,7 +8,7 @@ import org.apache.logging.log4j.Logger;
 import net.minecraft.item.ItemStack;
 
 /**
- * The recipe handler needs - to be discovered - a default constructor.
+ * The recipe handler implementation needs - to be discovered - a default constructor.
  *
  * @param The
  *            type of cached recipes the recipe handler produces/accepts
@@ -16,7 +16,7 @@ import net.minecraft.item.ItemStack;
 public interface RecipeHandler<T extends Recipe> {
 
 	/**
-	 * The unlocalized name of the recipe handler. It must be unique, and is used like an ID. Use dots in the name to group the recipe handlers.
+	 * The unlocalized name of the recipe handler. It must be unique, and will be used like an ID. Use dots in the name to group the recipe handlers.
 	 *
 	 * @return The unlocalized name
 	 */
@@ -60,17 +60,17 @@ public interface RecipeHandler<T extends Recipe> {
 	public Collection<T> getDynamicUsageRecipes(ItemStack ingredient);
 
 	/**
-	 * Called if the addon loads the recipe handler, but before the static recipes are loaded. All initializations should be done here, not in the constructor.
+	 * Called if the recipe handler is loaded, but before the static recipes are loaded. All initializations should be done here, not in a constructor.
 	 *
 	 * @param config
 	 *            An object that allows the recipe handler to access it's own configuration file section
 	 * @param logger
-	 *            The logger the addon assigned to this recipe handler
+	 *            The logger assigned to this recipe handler
 	 */
 	public void onPreLoad(RecipeHandlerConfiguration config, Logger logger);
 
 	/**
-	 * Called when the recipe handler is loaded; at this time all static recipes are loaded.
+	 * Called when the recipe handler is loaded; at this time all static recipes were loaded.
 	 *
 	 * @param staticRecipes
 	 *            The static recipes that were loaded for this handler. They've to be returned by getStaticRecipes()
@@ -92,10 +92,10 @@ public interface RecipeHandler<T extends Recipe> {
 	public int getComplicatedStaticRecipeDepth();
 
 	/**
-	 * Loads a static recipe that needs a combination of item stacks, to example an alloy forge recipe. Something like this is necessary if the Mod doesn't provide an IRecipe instance but a function like canSmelt(ItemStack ingredient, ItemStack
-	 * alloyItem) that determines the recipe dynamically. The addon then has to create all possible combinations of items and sends them to this method, which returns a cached recipe if a match was found. The count of item stacks in a combination (to
-	 * example two for the method above) is specified in getComplicatedStaticRecipeDepth(). Please note that itemcount^staticRecipeDepth iterations through all items are necessary to test all possible combinations, so this can be very expensive. If
-	 * possible, avoid this method of loading static recipes. This method can be called concurrently from multiple threads.
+	 * Loads a static recipe that needs a combination of item stacks, to example an alloy forge recipe. Something like this is necessary if a mod doesn't provide an IRecipe instance but a function like canSmelt(ItemStack ingredient, ItemStack
+	 * alloyItem) that determines the recipe dynamically. A collection of all possible item combinations has to be created which will be sent to this function, which returns a cached recipe if the input matches with a "recipe". The count of item
+	 * stacks in a combination (to example two for the method above) is specified in getComplicatedStaticRecipeDepth(). Please note that itemcount^staticRecipeDepth iterations through all items are necessary to test all possible combinations, so this
+	 * can be very expensive. If possible, avoid this method of loading static recipes. This method can be called concurrently from multiple threads.
 	 *
 	 * @param stacks
 	 *            An array containing a combination of getComplicatedStaticRecipeDepth() item stacks
@@ -104,8 +104,8 @@ public interface RecipeHandler<T extends Recipe> {
 	public T loadComplicatedStaticRecipe(ItemStack... stacks);
 
 	/**
-	 * Returns the URL of a file containing the current released version of the recipe handler. This will be used for the internal version checker of the addon, which notifies the user about new versions of the recipe handler. An internet connection
-	 * is needed for this. If null, the addon won't look for new versions.
+	 * Returns the URL of a file containing the current released version of the recipe handler. This will be used for an internal version checker, which notifies the user about new versions of the recipe handler. An internet connection is needed for
+	 * this. If the URL is null, no version checks will be made.
 	 *
 	 * @return An URL to a file containing informations about the current version of the recipe handler or null.
 	 */
